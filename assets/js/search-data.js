828ecdcda93237c75291ceb266a196c65e6b1770
Jiahong-2025 <jiahonglinu@gmail.com>
2026-05-20 17:22:19 +0000

    Deploying to gh-pages from @ Jiahong-2025/jiahong-2025.github.io@02375233d8bdbc4271a0214495fbc91093061fc7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-research",
-          title: "Research",
+        },{id: "nav-projects",
+          title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
           title: "CV",
@@ -29,6 +36,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Course materials, schedules, and resources for classes taught.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
           },
         },{id: "nav-people",
           title: "people",
